8678f486b91017fb586e968640b1c37196062425
wendelltmo <wendelltmo2306@outlook.com>
2024-07-18 09:38:17 -0300

    adding pipe exercise

--- pipe/pipe.c
+++ pipe/pipe.c
@@ -1,39 +1,37 @@
 #include <sys/wait.h>
+#include <string.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <stdio.h>
 #include <unistd.h>
 
-int count() {
-	return 100;
+int count(int x) {
+	return x + 100;
 }
 
 int main(int argc, char** argv) {
-	int fd[2], nbytes, sum;
-	char estado = 'p';
-	int my_pid = getpid();
-    	int child_pid;
-	pipe(fd);
-
-    	printf("%d: processos antes da criação do processo filho\n", my_pid);
-	if ((child_pid = fork()) == 0) {
-		/* close input */
-		close(fd[0]);
-        	my_pid = getpid();
-        	printf("%d: o valor da variável estado no processo filho é: %c\n", my_pid, estado);
-       		
-		write(fd[1], count(), 10);
-		exit(0);
-
-	} else {
-        	printf("%d: processos depois da criação do processo filho\n", my_pid);	
-        	printf("%d: pai está esperando pelo filho: %d\n", my_pid, child_pid);
-		/*closes output */
-		close(fd[1]);
-		wait(&child_pid);
-		nbytes = read(fd[0], sum, sizeof(sum));
-			
-	}
-
-    	return(0);
+    int input = atoi(argv[1]), num = 0, fd[2];
+    pid_t children[input];
+    pipe(fd);
+    for (int i = 0; i < input; i++) {
+        if((children[i] = fork()) == 0) {
+            if (i != 0) {
+                read(fd[0], &num, sizeof(num));
+            }
+            if (i == input -1) {
+                close(fd[0]);
+            }
+            num = count(num);
+            write(fd[1], &num, sizeof(num));
+            exit(0);
+        }
+    }
+    for (int i = 0; i < input; i++) {
+        waitpid(children[i], NULL, 0);
+    }
+/*close output*/
+	close(fd[1]);
+	read(fd[0], &num, sizeof(num));
+    printf("Sum: %d\n", num);
+    return(0);
 }
